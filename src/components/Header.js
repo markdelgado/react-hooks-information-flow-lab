464f292ad0e777dbdaabef9380b9d9692346aae9
Mark Delgado <markdelgado.md@hotmail.com>
2022-07-26 13:24:03 -0400

    commit tree 82e30649ded2ec2856f05c1a6a739e00fc5edf7c

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const header = () => {
+    
+    
+    
+    return(
+        <div>
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={handleDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header> <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      </div >
+    )
+}
